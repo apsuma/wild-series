6d718376b6d408166cd72cb24325c20a50c958c3
apsuma <delphine.belet@gmail.com>
2019-12-02 12:05:56 +0100

    fonctionnalités quete 13 ok actor created and screened

---
+++ src/Controller/ActorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ActorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/actor", name="actor_")
+ */
+class ActorController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(ActorRepository $actorRepository):Response
+    {
+        return $this->render('actor/index.html.twig', [
+            'actors' => $actorRepository->findAll(),
+        ]);
+    }
+}
